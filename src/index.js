714ba245e9503fbf00446c71a48663fde358fe27
Melisa Im <melisaevelynim@gmail.com>
2017-08-25 15:01:15 -0700

    Separated data source for quantity and menu items

--- src/index.js
+++ src/index.js
@@ -4,8 +4,39 @@ import './index.css';
 import OrderPage from './components/OrderPage';
 import data from './data/data';
 
+let orderItems = [
+	{
+		name: "Archer's Burger",
+		price: 8.99,
+		imagePath: '/img/burger.jpg',
+		id: 1,
+		quantity: 0
+	},
+	{
+		name: 'The 3.14',
+		price: 11.99,
+		imagePath: '/img/pizza.jpg',
+		id: 2,
+		quantity: 0
+	},
+	{
+		name: 'Babe',
+		price: 14.99,
+		imagePath: '/img/ribs.jpg',
+		id: 3,
+		quantity: 0
+	},
+	{
+		name: 'I Scream',
+		price: 7.99,
+		imagePath: '/img/ice_cream.jpg',
+		id: 4,
+		quantity: 0
+	}
+];
+
 function handleAddItem(itemId) {
-	data.menuItems.forEach(item => (item.id === itemId ? item.quantity++ : false));
+	orderItems.forEach(item => (item.id === itemId ? item.quantity++ : false));
 	render();
 }
 
@@ -13,7 +44,7 @@ function render() {
 	ReactDOM.render(
 		<OrderPage
 			menuItems={data.menuItems}
-			orderItems={data.menuItems}
+			orderItems={orderItems}
 			customerInfo={data.customerInfo}
 			onAddItem={handleAddItem}
 		/>,
